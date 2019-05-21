18dd21ecc18296e430a66ffcabb04186a163043d
wzshiming <wzshiming@foxmail.com>
2019-05-21 18:38:52 +0800

    change package

--- connect/connect.go
+++ connect/connect.go
@@ -5,12 +5,12 @@ import (
 	"net"
 
 	"github.com/wzshiming/bridge"
-	"github.com/wzshiming/httpconnect"
+	"github.com/wzshiming/httpproxy"
 )
 
 // CONNECT https?://[username:password@]{address}
 func CONNECT(dialer bridge.Dialer, address string) (bridge.Dialer, error) {
-	d, err := httpconnect.NewDialer(address)
+	d, err := httpproxy.NewDialer(address)
 	if err != nil {
 		return nil, err
 	}
